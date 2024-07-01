ab719fdc95b86a270b8d1559a9f993b99259e6af
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-07-01 21:37:06 +0530

    app files

---
+++ src/test/java/com/example/mythinking/MyThinkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mythinking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyThinkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
